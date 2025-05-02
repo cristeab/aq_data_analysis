609dd0f8fa9b2964fa77a2007419d9c158824917
Bogdan Cristea <cristeab@gmail.com>
2025-05-02 21:35:50 +0300

    working version

--- gas_resistance.py
+++ gas_resistance.py
@@ -9,14 +9,27 @@ URL = "http://192.168.77.81:8086"
 BUCKET = "temperature"
 TOKEN = os.environ.get("INFLUX_TOKEN")
 
-@st.cache
-def get_gas_resistance(time_range=20):
+@st.cache_data
+def get_gas_resistance(time_range=30):
     client = InfluxDBClient(url=URL, token=TOKEN, org=ORG)
     query_api = client.query_api()
     result_df = query_api.query_data_frame(f'from(bucket:"{BUCKET}") '
-    f'|> range(start: -{time_range}y) '
-    '|> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value") '
-    '|> keep(columns: ["_time","Measured Fluid", "T (degC)"])')
+    f'|> range(start: -{time_range}m) '
+    '|> filter(fn: (r) => r._measurement == "ambient_data") '
+    '|> pivot(rowKey:["_time"], columnKey: ["_field"], valueColumn: "_value")')
     client.close()
 
     return result_df
+
+chart_df = get_gas_resistance()
+# Convert gas resistance to kilo-ohms
+chart_df["gas"] = chart_df["gas"] / 1000
+# Round to one digit after the decimal point
+chart_df["gas"] = chart_df["gas"].round(1)
+
+chart_df.rename(columns={"_time": "Date-Time", "gas": "Gas Resistance (kOhms)"},
+                inplace=True)
+#print(chart_df)
+st.title("Gas Resistance")
+st.write("## Line Chart")
+st.line_chart(chart_df, x="Date-Time", y="Gas Resistance (kOhms)")
