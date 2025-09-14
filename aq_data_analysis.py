233a15ba4fcbdb94c52e6a88470d29eb62c4bfd9
Bogdan Cristea <cristeab@gmail.com>
2025-09-14 22:09:12 +0300

    updated code for the new database names

--- aq_data_analysis.py
+++ aq_data_analysis.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# streamlit run ./aq_data_analysis.py
 
 import os
 from datetime import datetime, timedelta, timezone
@@ -14,8 +15,14 @@ if not TOKEN:
     st.error("INFLUXDB3_AUTH_TOKEN environment variable is not set.")
     st.stop()
 
+def run_query(database, query):
+    client = InfluxDBClient3(host=URL, token=TOKEN, database=database, auth_scheme=AUTH_SCHEME)
+    df = client.query(query=query, language="sql", mode="pandas")
+    client.close()
+    return df
+
 @st.cache_data
-def get_data(bucket, measurement, minutes=30, stop_time_local=None):
+def get_data(database, table, minutes=30, stop_time_local=None):
     if stop_time_local is None:
         stop_time_local = datetime.now()
 
@@ -27,15 +34,22 @@ def get_data(bucket, measurement, minutes=30, stop_time_local=None):
     start_time_str = start_time_utc.isoformat().replace('+00:00', 'Z')
     stop_time_str = stop_time_utc.isoformat().replace('+00:00', 'Z')
 
-    client = InfluxDBClient3(host=URL, token=TOKEN, database=bucket, auth_scheme=AUTH_SCHEME)
+    # Query
     sql = (
-        f'SELECT * FROM "{measurement}" '
+        f'SELECT * FROM "{table}" '
         f'WHERE time >= \'{start_time_str}\' AND time <= \'{stop_time_str}\''
     )
-    result_df = client.query(query=sql, language="sql", mode="pandas")
-    client.close()
 
-    return result_df
+    return run_query(database, sql)
+
+def get_databases():
+    # there is no "SHOW DATABASES" command in InfluxDB 3.x, so we just hard code it here
+    return ["dust", "gas", "climate", "sound", "light"]
+
+def get_tables(database):
+    sql = ("SHOW TABLES")
+    df = run_query(database=database, query=sql)
+    return df.loc[df['table_schema'] == 'iox', 'table_name'].tolist()
 
 # Initialize session state for stop_date and stop_time
 if "stop_date" not in st.session_state:
@@ -64,39 +78,26 @@ stop_datetime = datetime.combine(st.session_state.stop_date, st.session_state.st
 # Add a slider to select the time range
 time_range_min = st.slider("Time Range (Minutes)", min_value=10, max_value=24*60, value=6*60, step=10)
 
-# Add combo box for selecting the bucket
-bucket_name = st.selectbox("Select Bucket", ["temperature", "noise", "aqi", "pm", "light"])
+# Add combo box for selecting the database (bucket)
+database_name = st.selectbox("Select Database", get_databases())
 
-# Select measurement based on the selected bucket
-if bucket_name == "temperature":
-    measurement = "ambient_data"
-elif bucket_name == "noise":
-    measurement = "noise_level"
-elif bucket_name == "aqi":
-    measurement = "air_quality_data"
-elif bucket_name == "pm":
-    sensor_index = st.selectbox("Select AQ Sensor", [0, 1])
-    measurement = f"air_quality_data_{sensor_index}"
-elif bucket_name == "light":
-    measurement = "light_data"
-else:    
-    st.error("No measurement for the selected bucket.")
-    st.stop()
+# Select table based on the selected database
+table_name = st.selectbox("Select Table", get_tables(database_name))
 
-chart_df = get_data(bucket=bucket_name, measurement=measurement, minutes=time_range_min, stop_time_local=stop_datetime)
+chart_df = get_data(database=database_name, table=table_name, minutes=time_range_min, stop_time_local=stop_datetime)
 
 # Filter columns to exclude those starting with "_" or named "result" or "table"
 valid_columns = [col for col in chart_df.columns if not col.startswith("_") and col not in ["result", "table", "time"]]
 
 # Add a combo box for selecting the column name
-column_name = st.selectbox(f"Select Column from bucket {bucket_name}", valid_columns)
+column_name = st.selectbox(f"Select column from table {table_name}", valid_columns)
 
 #ensure there is data for the selected time range
 if column_name not in chart_df.columns:
     st.error("No data found for the selected time range.")
     st.stop()
 
-if column_name == "gas":
+if column_name == "gas_resistance":
     # Convert gas resistance to kilo-ohms
     chart_df[column_name] = chart_df[column_name] / 1000
     pretty_name = "Gas Resistance (kOhms)"
